8e9d01c7e7901bba8fb8d2bf20e114f152a79d25
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-18 15:38:08 +0530

    Getting current day and month

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 ##################### Automated Birthday Wishing Email Project ######################
-# TODO 1 Starting files => 1)main.py 2)3 random letter tamplates 3)birthdays.csv containing name,email,birthday entries
+import datetime as dt
 
-#TODO 2 get current date and time and check if today matches a birthday in the birthdays.csv for every entry
+# TODO 1 Starting files => 1)main.py 2)3 random letter templates 3)birthdays.csv containing name,email,birthday entries
+
+# TODO 2 get current date and time and check if today matches a birthday in the birthdays.csv for every entry
+now=dt.datetime.now()
+month=now.month
+day=now.day
 
 
-#TODO 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from
-# birthdays.csv Send the letter generated to that person's email address.
+# TODO 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from
+#  birthdays.csv Send the letter generated to that person's email address.
