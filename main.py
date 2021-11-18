60e419c9578eade4a1aec09d8421343689932aca
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-18 15:15:39 +0530

    Started file

---
+++ main.py
@@ -0,0 +1,8 @@
+##################### Automated Birthday Wishing Email Project ######################
+# TODO 1 Starting files => 1)main.py 2)3 random letter tamplates 3)birthdays.csv containing name,email,birthday entries
+
+#TODO 2 get current date and time and check if today matches a birthday in the birthdays.csv for every entry
+
+
+#TODO 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from
+# birthdays.csv Send the letter generated to that person's email address.
